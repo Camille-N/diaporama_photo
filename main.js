66d97961f6341032320206a0f64c857fff88abb2
CamilleN <camillen@test.email>
2020-12-18 16:39:22 +0100

    add nav buttons

--- main.js
+++ main.js
@@ -13,18 +13,28 @@ const slider = document.body.querySelector('#slider');
 
 // Fonction pour afficher l'image
 function displayImg() {
-    // Création de l'image
+    
     for (let i = 0; i < images.length; i++) {
+        // Création de l'image
         const image = document.createElement("IMG");
         image.setAttribute("src", `img/${images[i]}`);
         image.setAttribute("width", "800");
         image.setAttribute("height", "533");
         image.className = "landscapeImg";
         slider.appendChild(image);
+
+        // Création des boutons de navigation
+        const rightButton = document.createElement("IMG");
+        rightButton.setAttribute("src", "arrow-right.webp");
+        slider.appendChild(rightButton);
+
+        const leftButton = document.createElement("IMG");
+        leftButton.setAttribute("src", "arrow-left.webp");
+        slider.appendChild(leftButton);
     }
 
     var landscapeImage = document.getElementsByClassName("landscapeImg");
-
 }
+
 
 displayImg();
